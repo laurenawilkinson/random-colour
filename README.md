8edb7d67af4632149b51297ce4cd87cd6eb9cdf1
laurenawilkinson <laurenwilkinson1998@gmail.com>
2018-05-28 15:07:27 +0100

    hexcode implemented

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # random-colour
 A simple random colour generator.
 
-> To do
-- make rgb values into a hex code
+## To do :sparkles:
+- ~~make rgb values into a hex code~~
 - add specific colour selectors
 - make the site prettier
--- random-hex.js
+++ random-hex.js
@@ -1,24 +1,42 @@
 let bg = document.getElementById('random-colour'),
     btnNewColour = document.getElementById('new-colour'),
     hexOutput = document.getElementById('random-hex'),
+    rgbOutput = document.getElementById('random-rgb'),
+    rgbColour,
+    hexColour,
     colour;
 
 randomColour();
 
 btnNewColour.addEventListener('click', randomColour);
 
+// insert selector queries here
+
 function randomColour(){
     let r = Math.floor(Math.random() * 255);
     let g = Math.floor(Math.random() * 255);
     let b = Math.floor(Math.random() * 255);
     
-    colour = `rgb(${r}, ${g}, ${b})`;
-    bg.style.backgroundColor = colour;
-    hexOutput.innerHTML = colour;
+    rgbColour = `rgb(${r}, ${g}, ${b})`;
+    hexColour = getHex(r, g, b);
     
-    console.log(colour);
+    bg.style.backgroundColor = hexColour;
+    hexOutput.innerHTML = hexColour;
+    rgbOutput.innerHTML = rgbColour;
 }
 
-function getHex(colour){
-    // translate rgb into hex
+function toHex(colourValue){
+    let hex = colourValue.toString(16);
+    console.log(hex);
+    
+    if (hex.length === 1){
+        return 0 + hex;
+    }
+    else{
+        return hex;
+    }
+}
+
+function getHex(r, g, b){
+    return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
 }
